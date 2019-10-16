91a813d2cf1e62aed8bcb2bd5bc1be5ed9166914
Amy Bemister <amy.bemister@gmail.com>
2019-10-16 21:19:16 +0000

    Completed function logic

--- letterPositions.js
+++ letterPositions.js
@@ -17,9 +17,20 @@ const assertArraysEqual = function(actual, expected) {
 };
 
 const letterPositions = function(sentence) {
-  const newSentence = sentence.replace(/\s/g, "");
   const results = {};
-  
+  for (let index = 0; index < sentence.length; index++) {
+    if (sentence[index] !== " "){
+    //do nothing
+    // Checking to see if the letter exists in results  
+    if (results[sentence[index]]) {
+    // if the letter exists push the index to the array 
+      results[sentence[index]].push(index);
+    } else {
+    // Creates a key and assigns the index to an array
+      results[sentence[index]] = [index];
+    }
+  }
+  } 
   return results;
 };
 
