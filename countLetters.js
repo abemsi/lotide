53777f795f3e15ec75ca080a451e582dbdf36585
Amy Bemister <amy.bemister@gmail.com>
2019-10-16 19:35:35 +0000

    Function so far that counts letters in a string

---
+++ countLetters.js
@@ -0,0 +1,34 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ ✅ ✅  Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑 🛑 🛑  Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  const results = {};
+  for (let letter of string) {
+    console.log(letter);
+      if (results[letter]) {
+        results[letter] += 1;
+      } else {
+        results[letter] = 1;
+      }
+  }
+  console.log(results);
+  return results;
+};
+
+assertEqual(countLetters("lighthouse in the house"), {
+  l: 1, 
+  i: 2,
+  g: 1,
+  h: 4,
+  t: 2,
+  o: 2,
+  u: 2,
+  s: 2,
+  e: 3,
+  n: 1,
+});
