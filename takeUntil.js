89492d898ac2aafde9631da8a8ee46a3f9264547
Amy Bemister <amy.bemister@gmail.com>
2019-10-17 22:33:27 +0000

    Function that returns a slice of an array until callback returns a truthy value

---
+++ takeUntil.js
@@ -0,0 +1,43 @@
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i <= array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  let match = eqArrays(actual, expected);
+  if (match) {
+    console.log(`✅ ✅ ✅  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 🛑 🛑  Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const takeUntil = function(array, callback) {
+  let results = [];
+  for (let item of array) {
+    console.log(item);
+    if (callback(item) === false) {
+      results.push(item);
+    } else {
+      break;
+    }
+  }
+  return results;
+}
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
