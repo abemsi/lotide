410642760774f0477b60fd5999d9025408886337
Amy Bemister <amy.bemister@gmail.com>
2019-10-21 19:28:10 +0000

    Refactored using mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,16 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
-const eqArrays = require('../eqArrays');
+// const assertArraysEqual = require('../assertArraysEqual');
+// const eqArrays = require('../eqArrays');
 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2]);
+describe("#middle", () => {
+  it ("should return 2 for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it ("should return [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+  it ("should not return 2 for [1, 2, 3, 4]", () => {
+    assert.notDeepEqual(middle([1, 2, 3, 4]), [2]);
+  });
+});
