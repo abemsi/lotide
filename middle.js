3920743cc8ee4e44313758cccd75f69c382fbee3
Amy Bemister <amy.bemister@gmail.com>
2019-10-21 16:53:48 +0000

    Refactored and modularized

--- middle.js
+++ middle.js
@@ -1,22 +1,5 @@
-const eqArrays = function(array1, array2) {
-  for (let i = 0; i <= array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const assertArraysEqual = require('./assertArraysEqual');
 
-const assertArraysEqual = function(actual, expected) {
-  let match = eqArrays(actual, expected);
-  if (match) {
-    console.log(`✅ ✅ ✅  Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 🛑 🛑  Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// Actual Function
 const middle = function(array) {
   let length = array.length;
   let result = [];
@@ -34,7 +17,4 @@ const middle = function(array) {
     return result;
 };
 
-// Test Code
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2]);
+module.exports = middle;
